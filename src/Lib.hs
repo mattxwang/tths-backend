ba6ff89ee39cbd6b301ebe6449b8201242fb43f1
Matthew Wang <matt@matthewwang.me>
2022-12-21 22:58:34 -0800

    trivial working example with hint
    see:
    
    - https://hackage.haskell.org/package/hint
    - https://github.com/haskell-hint/hint
    - https://github.com/haskell-hint/hint/blob/main/examples/example.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,38 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+
+import Data.List
+import Language.Haskell.Interpreter
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  r <- runInterpreter testHint
+  case r of
+    Left err -> putStrLn $ errorString err
+    Right () -> return ()
+
+testHint :: Interpreter ()
+testHint =
+  do
+    setImportsQ [("Prelude", Nothing)]
+    emptyLine
+    let expr1 = "[1,2] ++ [3]"
+    say $ "type of " ++ expr1 ++ " is:"
+    say =<< typeOf expr1
+    emptyLine
+
+
+errorString :: InterpreterError -> String
+errorString (WontCompile es) = intercalate "\n" (header : map unbox es)
+  where
+    header = "ERROR: Won't compile:"
+    unbox (GhcError e) = e
+errorString e = show e
+
+say :: String -> Interpreter ()
+say = liftIO . putStrLn
+
+emptyLine :: Interpreter ()
+emptyLine = say ""
