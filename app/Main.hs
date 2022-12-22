3af436cf0f1a1b18d768b74aa72d070eea30a535
Matthew Wang <matt@matthewwang.me>
2022-12-21 23:15:58 -0800

    trivial scotty setup
    see:
    - https://seanhess.github.io/2015/08/19/practical-haskell-json-api.html
    - https://github.com/scotty-web/scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
+
+import Web.Scotty
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  someFunc
+  putStrLn "Starting Server..."
+  scotty 3000 $ do
+      get "/hello" $ do
+          text "hello world!"
